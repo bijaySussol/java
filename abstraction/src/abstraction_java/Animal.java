4f4b51e0d9908f317e3a3fc8c7ed88fdf34dbc8b
bijaySussol <bijaypunmagar@gmail.com>
2024-05-12 15:12:31 +0100

    project setup

---
+++ abstraction/src/abstraction_java/Animal.java
@@ -0,0 +1,7 @@
+package abstraction_java;
+
+public class Animal {
+	public Animal() {
+		System.out.println("Animal constructor");
+	}
+}
---
+++ abstraction/src/abstraction_java/Cat.java
@@ -0,0 +1,7 @@
+package abstraction_java;
+
+public class Cat extends Animal{
+	public Cat() {
+		System.out.println("Cat Constructor");
+	}
+}
