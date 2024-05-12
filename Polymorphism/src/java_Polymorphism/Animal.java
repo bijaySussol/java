63d5c900749c36c5fc9583396c44ba11be292586
bijaySussol <bijaypunmagar@gmail.com>
2024-05-12 20:11:56 +0100

    polymorphism

---
+++ Polymorphism/src/java_Polymorphism/Animal.java
@@ -0,0 +1,22 @@
+package java_Polymorphism;
+
+public class Animal {
+	private String name;
+	private String color;
+	
+	public void eat() {
+		// TODO Auto-generated method stub
+		System.out.println("Animal Eating");
+	}
+	
+	public String getName() {
+		// TODO Auto-generated method stub
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
