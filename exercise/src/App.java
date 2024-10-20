84cc57b1eddfbb2e499d5c8cd116ad8b8215cc4b
bijaySussol <bijaypunmagar@gmail.com>
2024-10-20 11:25:50 +0100

    project setup

--- exercise/src/App.java
+++ exercise/src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        int total_count = 5;
+        for (int i = 0; i <= total_count; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("x ");
+                i += 1;
+            }
+        }
     }
 }
