cb9c9d4737e549ab35d56d895632519049df9653
bijaySussol <bijaypunmagar@gmail.com>
2024-10-20 18:34:25 +0100

    logic

--- sumEvenNumber/sumEvenNumber/src/App.java
+++ sumEvenNumber/sumEvenNumber/src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int number = 4;
+        int result = 0;
+        for (int i = 0; i < number; i++) {
+            result = result + (2 * i);
+        }
+        System.out.println("The su of even number " + result);
     }
 }
