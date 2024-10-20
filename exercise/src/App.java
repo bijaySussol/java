d12b8cde3a1aaeeb5856f44359958955841b4113
bijaySussol <bijaypunmagar@gmail.com>
2024-10-20 11:35:26 +0100

    8 - logic appied

--- exercise/src/App.java
+++ exercise/src/App.java
@@ -1,12 +1,11 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
         int total_count = 5;
-        for (int i = 0; i <= total_count; i++) {
+        for (int i = total_count; i > 0; i--) {
             for (int j = 0; j < i; j++) {
-                System.out.print("x ");
-                i += 1;
+                System.out.print("* ");
             }
+            System.out.print("\n");
         }
     }
 }
