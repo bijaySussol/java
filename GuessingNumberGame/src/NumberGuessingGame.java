e7235200e3097e16ac62d84d4002de29f07db897
bijaySussol <bijaypunmagar@gmail.com>
2024-10-22 21:31:39 +0100

    project skeleton

---
+++ GuessingNumberGame/src/NumberGuessingGame.java
@@ -0,0 +1,13 @@
+/**
+ * Implementing the guessing game no limitation on retries, but can quit the game by typing "quit" from the console else enjoy!
+ * 
+ **/
+public class NumberGuessingGame {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello from the NumberGuessingGame");
+		
+	}
+
+}
