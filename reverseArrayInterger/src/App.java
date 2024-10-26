af496f6d69e8848dcedf1f791fcba940ccae7332
bijaySussol <bijaypunmagar@gmail.com>
2024-10-26 12:33:30 +0100

    logic

--- reverseArrayInterger/src/App.java
+++ reverseArrayInterger/src/App.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ArrayList<Integer> arguments = new ArrayList<>();
+        arguments.add(5);
+        arguments.add(4);
+        arguments.add(3);
+        arguments.add(2);
+        arguments.add(1);        
+        if (!arguments.isEmpty()) {
+            reverseNumber(arguments);
+        } else {
+            System.out.println("No arguments specified!");
+        }
+    }
+    static void reverseNumber(ArrayList<Integer> arguments) {
+            Collections.reverse(arguments);
+            System.out.println(arguments);
     }
 }
