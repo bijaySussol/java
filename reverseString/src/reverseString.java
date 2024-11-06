a0ab622afed34248f9ad7064d09f857331069c7d
bijaySussol <bijaypunmagar@gmail.com>
2024-11-06 11:33:27 +0000

    loguc

--- reverseString/src/reverseString.java
+++ reverseString/src/reverseString.java
@@ -1,5 +1,20 @@
-public class reverseString {
+public class ReverseString {
+    String argument;
+        public ReverseString(String value) {
+            if (value.equals("")) {
+                value = "Apple";// Default value
+            }
+            argument = value;
+    };
+    static void stringReverse(String value) {
+        int len = value.length()-1;
+        for (int i = len; i >= 0; i--) {
+            char result = value.charAt(i);
+            System.out.print(result);
+        }
+    }
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ReverseString r = new ReverseString("Tiger");
+        stringReverse(r.argument);
     }
 }
