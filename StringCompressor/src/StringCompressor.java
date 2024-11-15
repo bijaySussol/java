c64c89e2cdac80715d141a49cdc713d8979057cd
bijaySussol <bijaypunmagar@gmail.com>
2024-11-15 20:38:27 +0000

    return unique word

--- StringCompressor/src/StringCompressor.java
+++ StringCompressor/src/StringCompressor.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+
 public class StringCompressor {
+
+    private static ArrayList<Character> getUniqueWords(char[] words) {
+        ArrayList<Character> uniqueWordCharacters = new ArrayList<Character>();
+        for (char thisWordCharacter : words) {
+            if (!uniqueWordCharacters.contains(thisWordCharacter)) {
+                uniqueWordCharacters.add(thisWordCharacter);
+            }
+        }
+        return uniqueWordCharacters;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, StringCompressor!");
+        System.out.println(getUniqueWords("apple".toCharArray()));
     }
 }
