99408ea1a8da9b2ea34d9f1a9935402999b02f83
bijaySussol <bijaypunmagar@gmail.com>
2024-12-03 21:56:52 +0000

    logic

--- myArrayV2/src/Main.java
+++ myArrayV2/src/Main.java
@@ -1,5 +1,44 @@
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        String[] names = {"Milan", "Marco", "Lucy", "Alexandra"};
+        String[] longerThan = getLongerThan(names, 8);
+        System.out.println(Arrays.toString(longerThan));
+        System.out.println(Arrays.toString(getLongerThan(names, 3)));
+        System.out.println(Arrays.toString(getLongerThan(names, 5)));
+        MyArray myArray = new MyArray();
+        myArray.add("Lucy");
+        myArray.add("Milan");
+        myArray.add("DEF");
+        myArray.add("ABC");
+        System.out.println("I have below elements in arrays:");
+        for (int i = 0; i < myArray.elements.length; i++) {
+            System.out.println(myArray.elements[i]);
+        }
+        System.out.println("Found elements are: "+myArray.get(myArray.elements,2));
+        System.out.println("Found elements are: "+myArray.size(myArray.elements));
+
+        // myArray.get(5);
+        // myArray.size();
+    }
+
+    private static String[] getLongerThan(String[] strings, int length) {
+        int count = 0;
+        for (int i = 0; i < strings.length; i++) {
+            if (strings[i].length() >= length) {
+                count++;
+            }
+        }
+        String[] result = new String[count];
+
+        int index = 0;
+        for (int i = 0; i < strings.length; i++) {
+            if (strings[i].length() >= length) {
+                result[index++] = strings[i];
+            }
+        }
+
+        return result;
     }
 }
--- myArrayV2/src/MyArray.java
+++ myArrayV2/src/MyArray.java
@@ -1,3 +1,32 @@
 public class MyArray {
-    
+    String[] elements;
+    public int length;
+
+    public void add(String element) {
+
+        if (elements == null) {
+            elements = new String[1];
+            elements[0] = element;
+        } else {
+            String[] newElements = new String[elements.length + 1];
+            for (int i = 0; i < elements.length; i++) {
+                newElements[i] = elements[i];
+            }
+            newElements[elements.length] = element;
+            elements = newElements;
+        }
+    }
+    public String get(String [] elements, int index) {
+        return (index > elements.length-1) ? "Out Of Bound": elements[index];
+}
+
+
+    public int size(String [] elements) {
+        return elements.length;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
